4fb0fefcd3780126f06957ea9426cfc94acf49ed
none <alexander.podkidyshev@gmail.com>
2012-07-03 12:22:29 +0300

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Substack::Application.config.secret_token = '199cfe4090b7b00770745d26a5618bd2a561907ed8127491079ca09f8887c1946fda72fd4017f6797971350dec127b82e24cc2727576e366fce8a3c48649a055'
